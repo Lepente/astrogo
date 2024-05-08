ef429126d2ca1e1ee951571a857829db4df74d5d
Lepente <molnar.levente773@gmail.com>
2024-05-08 12:32:37 +0300

    fixes - navbar, responsiveness

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = bootstrap.Collapse.getOrCreateInstance(menuToggle, {toggle: false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
